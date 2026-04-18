5b40b04648c0f38f49efeea68f4040d8d751e5af
Hussain Taheri <hussaint786@icloud.com>
2026-04-17 21:18:03 -0400

    build game card component with fake team data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,25 @@
 import GameCard from "./components/GameCard.jsx";
 
 function App() {
-  return <GameCard />;
+  return (
+    <div className = "bg-[#1A1A1A] min-h-screen">
+      <GameCard 
+      home_team_img = "https://upload.wikimedia.org/wikipedia/sco/0/01/Golden_State_Warriors_logo.svg"
+        home_team_abv = "GSW"
+        home_team_score = "50"
+        home_team_wins = "73"
+        home_team_loses = "9"
+
+        period = "2"
+        game_clock = "4:37"
+
+        away_team_img = "https://upload.wikimedia.org/wikipedia/commons/3/3c/Los_Angeles_Lakers_logo.svg"
+        away_team_abv = "LAL"
+        away_team_score = "42"
+        away_team_wins = "40"
+        away_team_loses = "42"
+      />
+    </div>
+  )
 }
 export default App
--- frontend/src/components/GameCard.jsx
+++ frontend/src/components/GameCard.jsx
@@ -1,6 +1,30 @@
-function GameCard() {
+function GameCard(props) {
     return (
-        <h1>Hello World</h1>
+        <div className = "flex justify-between">
+            <div className = "flex flex-col">
+                <div className = "flex items-center">
+                    <img src = {props.home_team_img} alt = "home team logo" height = "75" width = "75"></img>
+                    <h1>{props.home_team_score}</h1>
+                </div>
+                <h2>{props.home_team_abv}</h2>
+                <p>({props.home_team_wins} - {props.home_team_loses})</p>
+            </div>
+            
+            <div className = "flex flex-col">
+                <h1>VS</h1>
+                <h3>Q{props.period} - {props.game_clock}</h3>
+            </div>
+
+            <div className = "flex flex-col">
+                <div>
+                    <img src = {props.away_team_img} alt = "away team logo"  height = "75" width = "75"></img>
+                    <h1>{props.away_team_score}</h1>
+                </div>
+                <h2>{props.away_team_abv}</h2>
+                <p>({props.away_team_wins} - {props.away_team_loses})</p>
+            </div>
+        </div>
     )
 }
+
 export default GameCard
