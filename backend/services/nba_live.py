6a9ae09ce1ae4cc4dd81f6cf3751c75e3a45fbaf
Hussain Taheri <hussaint786@icloud.com>
2026-04-28 00:00:28 -0400

    add function to get live box score stats

--- backend/services/nba_live.py
+++ backend/services/nba_live.py
@@ -1,5 +1,6 @@
 from nba_api.live.nba.endpoints.scoreboard import ScoreBoard
 from nba_api.live.nba.endpoints.playbyplay import PlayByPlay
+from nba_api.live.nba.endpoints.boxscore import BoxScore
 
 scoreboard = ScoreBoard()
 
@@ -20,6 +21,39 @@ def get_live_play_by_play(game_id):
     our_data["clock"] = dataset[-1]["clock"]
     our_data["actionType"] = dataset[-1]["actionType"]
     return our_data
+
+def get_box_score(game_id):
+    data = BoxScore(game_id).get_dict()
+    home_data = {}
+    away_data = {}
+
+    home_data["fieldGoals"] = str(data["game"]["homeTeam"]["statistics"]["fieldGoalsMade"]) + "/" + str(data["game"]["homeTeam"]["statistics"]["fieldGoalsAttempted"])
+    home_data["fieldGoalsPercentage"] = data["game"]["homeTeam"]["statistics"]["fieldGoalsPercentage"]
+    home_data["threePointers"] = str(data["game"]["homeTeam"]["statistics"]["threePointersMade"]) + "/" + str(data["game"]["homeTeam"]["statistics"]["threePointersAttempted"])
+    home_data["threePointersPercentage"] = data["game"]["homeTeam"]["statistics"]["threePointersPercentage"]
+    home_data["freeThrows"] = str(data["game"]["homeTeam"]["statistics"]["freeThrowsMade"]) + "/" + str(data["game"]["homeTeam"]["statistics"]["freeThrowsAttempted"])
+    home_data["freeThrowsPercentage"] = data["game"]["homeTeam"]["statistics"]["freeThrowsPercentage"]
+    home_data["rebounds"] =  data["game"]["homeTeam"]["statistics"]["reboundsTotal"]
+    home_data["steals"] =  data["game"]["homeTeam"]["statistics"]["steals"]
+    home_data["assists"] =  data["game"]["homeTeam"]["statistics"]["assists"]
+    home_data["blocks"] =  data["game"]["homeTeam"]["statistics"]["blocks"]
+    home_data["turnovers"] =  data["game"]["homeTeam"]["statistics"]["turnoversTotal"]
+    home_data["fouls"] =  data["game"]["homeTeam"]["statistics"]["foulsTeam"]
+
+    away_data["fieldGoals"] = str(data["game"]["awayTeam"]["statistics"]["fieldGoalsMade"]) + "/" + str(data["game"]["awayTeam"]["statistics"]["fieldGoalsAttempted"])
+    away_data["fieldGoalsPercentage"] = data["game"]["awayTeam"]["statistics"]["fieldGoalsPercentage"]
+    away_data["threePointers"] = str(data["game"]["awayTeam"]["statistics"]["threePointersMade"]) + "/" + str(data["game"]["awayTeam"]["statistics"]["threePointersAttempted"])
+    away_data["threePointersPercentage"] = data["game"]["awayTeam"]["statistics"]["threePointersPercentage"]
+    away_data["freeThrows"] = str(data["game"]["awayTeam"]["statistics"]["freeThrowsMade"]) + "/" + str(data["game"]["awayTeam"]["statistics"]["freeThrowsAttempted"])
+    away_data["freeThrowsPercentage"] = data["game"]["awayTeam"]["statistics"]["freeThrowsPercentage"]
+    away_data["rebounds"] =  data["game"]["awayTeam"]["statistics"]["reboundsTotal"]
+    away_data["steals"] =  data["game"]["awayTeam"]["statistics"]["steals"]
+    away_data["assists"] =  data["game"]["awayTeam"]["statistics"]["assists"]
+    away_data["blocks"] =  data["game"]["awayTeam"]["statistics"]["blocks"]
+    away_data["turnovers"] =  data["game"]["awayTeam"]["statistics"]["turnoversTotal"]
+    away_data["fouls"] =  data["game"]["awayTeam"]["statistics"]["foulsTeam"]
+
+    return home_data, away_data
 
 
 
