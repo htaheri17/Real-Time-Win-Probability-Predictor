238f00d4a04ef52dd06656b5fc4e35241226db5e
Hussain Taheri <hussaint786@icloud.com>
2026-04-28 00:07:23 -0400

    add React Router within frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,17 @@
 import GameCard from "./components/GameCard.jsx";
 import Navbar from "./components/Navbar.jsx";
+import { Routes, Route } from "react-router-dom";
+import GameDetail from "./components/GameDetail.jsx";
+import DashBoard from "./components/DashBoard.jsx";
 
 function App() {
   return (
     <div className = "bg-[#1A1A1A] min-h-screen">
       <Navbar />
-      <GameCard 
-      home_team_img = "https://upload.wikimedia.org/wikipedia/sco/0/01/Golden_State_Warriors_logo.svg"
-        home_team_abv = "GSW"
-        home_team_score = "50"
-        home_team_wins = "73"
-        home_team_loses = "9"
-
-        period = "2"
-        game_clock = "4:37"
-
-        away_team_img = "https://upload.wikimedia.org/wikipedia/commons/3/3c/Los_Angeles_Lakers_logo.svg"
-        away_team_abv = "LAL"
-        away_team_score = "42"
-        away_team_wins = "40"
-        away_team_loses = "42"
-      />
+      <Routes>
+        <Route path = "/testgameid" element = { <GameDetail /> } />
+        <Route path = "/" element = { <DashBoard /> } />
+      </Routes>
     </div>
   )
 }
--- frontend/src/components/GameCard.jsx
+++ frontend/src/components/GameCard.jsx
@@ -1,30 +1,32 @@
+import { Link } from 'react-router';
 function GameCard(props) {
     return (
-        <div className = "flex justify-between items-center p-6 bg-[#232323] shadow-xl/30 rounded-xl py-8 px-8 m-8 w-5/6 mx-auto">
-            <div className = "flex flex-col">
-                <div className = "flex items-center">
-                    <img src = {props.home_team_img} alt = "home team logo" height = "75" width = "75"></img>
-                    <h1>{props.home_team_score}</h1>
+        <Link to = "/testgameid">
+            <div className = "flex justify-between items-center p-6 bg-[#232323] shadow-xl/30 rounded-xl py-8 px-8 m-8 w-5/6 mx-auto hover:border border-[#22CE83]">
+                <div className = "flex flex-col">
+                    <div className = "flex items-center">
+                        <img src = {props.home_team_img} alt = "home team logo" height = "75" width = "75"></img>
+                        <h1>{props.home_team_score}</h1>
+                    </div>
+                    <h2>{props.home_team_abv}</h2>
+                    <p>({props.home_team_wins} - {props.home_team_loses})</p>
                 </div>
-                <h2>{props.home_team_abv}</h2>
-                <p>({props.home_team_wins} - {props.home_team_loses})</p>
-            </div>
-            
-            <div className = "flex flex-col">
-                <h1>VS</h1>
-                <h3>Q{props.period} - {props.game_clock}</h3>
-            </div>
+                
+                <div className = "flex flex-col">
+                    <h1>VS</h1>
+                    <h3>Q{props.period} - {props.game_clock}</h3>
+                </div>
 
-            <div className = "flex flex-col">
-                <div>
-                    <img src = {props.away_team_img} alt = "away team logo"  height = "75" width = "75"></img>
-                    <h1>{props.away_team_score}</h1>
+                <div className = "flex flex-col">
+                    <div className = "flex items-center">
+                        <h1>{props.away_team_score}</h1>
+                        <img src = {props.away_team_img} alt = "away team logo"  height = "75" width = "75"></img>
+                    </div>
+                    <h2>{props.away_team_abv}</h2>
+                    <p>({props.away_team_wins} - {props.away_team_loses})</p>
                 </div>
-                <h2>{props.away_team_abv}</h2>
-                <p>({props.away_team_wins} - {props.away_team_loses})</p>
             </div>
-        </div>
+        </Link>
     )
 }
-
 export default GameCard
