e3a4e58890e9b18de0d971ae012c0b269b964554
Hussain Taheri <hussaint786@icloud.com>
2026-05-04 22:28:23 -0400

    make a connection to the database

---
+++ backend/database/connection.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+def get_connection():
+    try:
+        # make the connection to the database
+        connection = psycopg2.connect(
+            user = "postgres",
+            password = "",
+            host = "localhost",
+            port = "5432",
+            database = "prediction_history"
+        )
+        print("You are now connected to prediction_history database")
+        return connection
+        
+    # if we werent able to connect to the db than well print the error
+    except (Exception, psycopg2.Error) as error:
+        print(f"Error while connecting to PostgreSQL: {error}")
+
+def close_connection(connection, cursor):
+    # always close the cursor and application
+    if connection:
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
